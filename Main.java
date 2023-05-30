22a66d42a0221dbd0a599d37f79d2b0c2bf1e246
Eder David Barrero Castañeda <ederd.barreroc@konradlorenz.edu.co>
2023-05-30 18:40:49 +0000

    Main

---
+++ Main.java
@@ -0,0 +1,18 @@
+/*
+ */
+package Presentacion;
+
+import java.io.IOException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+
+/**
+ *
+ * @author daveb
+ */
+public class Main {
+ public static void main(String[] args) throws IOException, InvalidFormatException {
+Salidas salida = new Salidas();
+salida.mostrarResultado();
+}
+}
+
